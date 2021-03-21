5e05f800cd3827db61925ce7c07c96516d58f85e
Rutorg <rutorgnz@gmail.com>
2021-03-21 12:11:21 +0700

    Добавил все файлы

---
+++ MyDict.h
@@ -0,0 +1,289 @@
+#pragma once
+#include <assert.h>
+
+template <typename keyT, typename valT>
+class MyDict
+{
+public:
+	struct Node;
+
+	// Конструктор по умолчанию.
+	// Результат: будет создан пустой словарь.
+	MyDict()
+	{
+
+	}
+
+
+	// Деструктор.
+	// Если в списке хранились указатели, то удаляются только эти указатели, не информация
+	// на которую они указывают.
+	// Результат: будут удалены все узлы.
+	~MyDict()
+	{
+		clear();
+	}
+
+
+	// Очистить.
+	// Если в списке хранились указатели, то удаляются только эти указатели, не информация
+	// на которую они указывают.
+	// Результат: будут удалены все узлы в списке, кроме барьерного.
+	void clear()
+	{
+
+	}
+	
+
+	Node* getRootPtr()
+	{
+		return m_root;
+	}
+
+
+	void insert(keyT key, valT value)
+	{
+		if (m_root == nullptr) {
+			m_root = new Node(nullptr, key, value);
+			m_root->isRed = false; // Корень должен быть черным.
+			return;
+		}
+
+		bool isInsertedLeft = false;
+		Node* parentNode = searchParent(m_root, key, isInsertedLeft);
+
+		Node* insertingNode = new Node(parentNode, key, value);
+		parentNode->setChild(insertingNode, isInsertedLeft);
+
+		balance(insertingNode);
+	}
+
+
+	void erase(int m_value)
+	{
+
+	}
+
+	// Возвращает указатель на узел с m_value. Если nullptr, то значит нет такого узла.
+	Node* search(Node* startNode, keyT key)
+	{
+		// Текущий узел - искомый.
+		if (startNode == nullptr || startNode->m_key == key) {
+			return startNode;
+		}
+
+		return search( startNode->getChild(startNode->isKeyLeft(key)), key);
+	}
+
+	Node* searchParent(Node* startNode, keyT key, bool& isChildLeft)
+	{
+		isChildLeft = startNode->isKeyLeft(key);
+		Node* nextNode = startNode->getChild(isChildLeft);
+
+		// Следующий узел - искомый.
+		if (nextNode == nullptr || startNode->m_key == key) {
+			return startNode;
+		}
+
+
+		return searchParent(nextNode, key, isChildLeft);
+	}
+
+
+	class Node
+	{
+	public:
+		// Красный по умолчанию.
+		// Потомки - nullptr.
+		// m_key = 0
+		Node(Node* parent) : parent(parent)
+		{
+			m_key = {};
+			m_value = {};
+
+			isRed = true;
+
+			left = nullptr;
+			right = nullptr;
+		}
+
+		// Красный по умолчанию.
+		// Потомки - nullptr.
+		Node(Node* parent, keyT key, valT value) : Node(parent)
+		{
+			m_key = key;
+			m_value = value;
+		}
+
+		bool isKeyLeft(keyT key)
+		{
+			return (key < m_key);
+		}
+
+		bool isLeft()
+		{
+			return parent->isKeyLeft(m_key);
+		}
+
+
+		Node* getParent()
+		{
+			return parent;
+		}
+
+		Node* getChild(bool isLeft)
+		{
+			return ( (isLeft) ? (left) : (right) );
+		}
+
+		Node* getSibling()
+		{
+			return parent->getChild(!isLeft());
+		}
+
+		Node* getUncle()
+		{
+			return parent->getSibling();
+		}
+
+
+		void setChild(Node* newChild, bool isNewChildLeft)
+		{
+			( (isNewChildLeft) ? (left) : (right) ) = newChild;
+			if (newChild != nullptr) {
+				newChild->parent = this;
+			}
+		}
+
+		void makeRoot(Node*& treeRoot)
+		{
+			treeRoot = this;
+			parent = nullptr;
+			isRed = false;
+		}
+
+
+		void assertRelations()
+		{
+			if (this == nullptr) return;
+			if (parent != nullptr) assert(parent->getChild(isLeft()) == this);
+			if (left != nullptr) assert(left->parent == this);
+			if (right != nullptr) assert(right->parent == this);
+		}
+
+
+
+		// Ключ.
+		keyT m_key;
+
+		// Информация хранящаяся в узле.
+		valT m_value;
+
+		// Красный ли узел?
+		bool isRed;
+
+	private:
+
+		// Указатель на родителя.
+		Node* parent;
+
+		// Указатель левого потомка.
+		Node* left;
+
+		// Указатель на правого потомка.
+		Node* right;
+	};
+
+private:
+
+	void balance(Node* inserted)
+	{
+		assert(inserted != m_root);
+
+		Node* parent = inserted->getParent();
+
+		// 1. Если родительский узел черный, то закончили.
+		if ( !(parent->isRed) ) {
+			return;
+		}
+
+		/*======В данный момент и дальше известно, что вставленный и родитель красные=====*/
+
+		Node* grandParent = parent->getParent();
+		Node* uncle = inserted->getUncle();
+		
+		/* 2. Если дядя красный, то перекрашиваем
+			дядю и родителя в черный, а деда в красный, если дед не корень,
+			если корень - не красим его и закончили. */
+		if (uncle != nullptr && uncle->isRed) {
+			uncle->isRed = false;
+			parent->isRed = false;
+
+			// если дед - корень, то закончили.
+			if (grandParent == m_root) {
+				return;
+			}
+
+			// иначе перекрашиваем его и проводим балансировку для него и закончили.
+			grandParent->isRed = true;
+			balance(grandParent);
+			return;
+		}
+
+
+		// 3. Нужно делать вращения и перекрашивания.
+
+		const bool wasParentLeft = parent->isLeft();
+		const bool wasInsertedLeft = inserted->isLeft();
+
+		// если нужно сделать первое вращение.
+		if (wasParentLeft != wasInsertedLeft) {
+			rotate(parent, wasParentLeft);
+
+			inserted->isRed = false; // Средним звеном становится вставленное звено. Оно должно быть черным.
+		}
+		else {
+			parent->isRed = false; // Иначе среднее звено остатся средним. Оно должно быть черным.
+		}
+
+		// второе вращение.
+		rotate(grandParent, !wasParentLeft);
+
+		// Перекрашивание. Дед уходит вниз, должен стать красным.
+		grandParent->isRed = true;
+	}
+
+
+	// Занимается всеми связями (parent, left, right).
+	void rotate(Node* rotatedNode, bool isLeftRotation) 
+	{
+		Node* parentNode = rotatedNode->getParent(); // Родитель узла.
+		Node* targetChildNode = rotatedNode->getChild(!isLeftRotation); // Ребенок узла.
+		Node* lostGrandChild = targetChildNode->getChild(isLeftRotation); // Узел, который будет "утерян" при повороте.
+
+		// Заботимся о связи родителя с внуком.
+		if (rotatedNode == m_root) {
+			// Новый корень.
+			targetChildNode->makeRoot(m_root);
+		}
+		else {
+			// Ставим родителю в качестве ребенка внука (на место вращаемого).
+			parentNode->setChild(targetChildNode, rotatedNode->isLeft());
+		}
+
+		// Ребенок теперь родитель вращаемого узла.
+		targetChildNode->setChild(rotatedNode, isLeftRotation);
+
+		// Назначаем вращаемому "утерянный" узел.
+		rotatedNode->setChild(lostGrandChild, !isLeftRotation);
+
+		// DEBUG.
+		parentNode->assertRelations();
+		rotatedNode->assertRelations();
+		targetChildNode->assertRelations();
+
+	}
+
+	// Корень дерева.
+	Node* m_root;
+};
---
+++ main.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+#include <io.h>
+#include <fcntl.h>
+#include <windows.h>
+#include <random>
+
+#include "MyDict.h"
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+//secondary function
+void draw_tree_hor2(MyDict<int, double>::Node* tree, int depth, char* path, int right, HANDLE hConsole)
+{
+	const int space = 2;
+
+	// stopping condition
+	if (tree == nullptr)
+		return;
+
+	// increase spacing
+	depth++;
+
+	// start with right node
+	draw_tree_hor2(tree->getChild(false), depth, path, 1, hConsole);
+
+	if (depth > 1)
+	{
+		// set | draw map
+		path[depth - 2] = 0;
+
+		if (right)
+			path[depth - 2] = 1;
+	}
+
+	if (tree->getChild(true))
+		path[depth - 1] = 1;
+
+
+	// print root after spacing
+	printf("\n");
+
+	for (int i = 0; i < depth - 1; i++)
+	{
+		if (i == depth - 2)
+			printf("+");
+		else if (path[i])
+			printf("|");
+		else
+			printf(" ");
+
+		for (int j = 1; j < space; j++)
+			if (i < depth - 2)
+				printf(" ");
+			else
+				printf("-");
+	}
+
+	// Если цвет красный, то ставим его красным.
+	if (tree->isRed) {
+		SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+	}
+	std::cout << tree->m_key << endl;
+	SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+
+	// vertical spacers below
+	for (int i = 0; i < depth; i++)
+	{
+		if (path[i])
+			printf("|");
+		else
+			printf(" ");
+
+		for (int j = 1; j < space; j++)
+			printf(" ");
+	}
+
+	// go to left node
+	draw_tree_hor2(tree->getChild(true), depth, path, 0, hConsole);
+}
+//primary fuction
+void draw_tree_hor(MyDict<int, double>::Node* tree)
+{
+	HANDLE hConsole;
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	// should check if we don't exceed this somehow..
+	char path[255] = {};
+
+	//initial depth is 0
+	draw_tree_hor2(tree, 0, path, 0, hConsole);
+}
+
+
+void test1()
+{
+	srand(static_cast<unsigned int>(time(0)));
+	MyDict<int, double> tree;
+
+	std::vector<int> vec(30);
+	for (size_t i = 0; i < vec.size(); i++) {
+		vec[i] = i;
+	}
+	std::random_shuffle(vec.begin(), vec.end());
+
+	for (auto& element : vec) {
+		cout << element << ' ';
+		tree.insert(element, 0.0);
+	}
+	cout << endl;
+
+	draw_tree_hor(tree.getRootPtr());
+	cout << endl;
+}
+
+void test2()
+{
+	MyDict<int, double> tree;
+
+	tree.insert(10, 0.0);
+	tree.insert(18, 0.0);
+	tree.insert(7, 0.0);
+	tree.insert(15, 0.0);
+	tree.insert(16, 0.0);
+	tree.insert(30, 0.0);
+	tree.insert(25, 0.0);
+	tree.insert(40, 0.0);
+	tree.insert(60, 0.0);
+	tree.insert(2, 0.0);
+	tree.insert(1, 0.0);
+	tree.insert(70, 0.0);
+
+
+
+	draw_tree_hor(tree.getRootPtr());
+}
+
+
+int main()
+{
+	test1();
+	test2();
+
+	return 0;
+}
